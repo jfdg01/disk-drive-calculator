f38507d83fbf4e3979e2341fb66910ee5a6daae0
Javier Francisco Dibo Gomez <jfdg0002@red.ujaen.es>
2024-11-27 13:43:12 +0100

    Captured Zhu Yuan data

--- source/main.py
+++ source/main.py
@@ -1,5 +1,7 @@
 from lib2to3.pytree import convert
 
+from source.disk_database import DiskDatabase
+from source.disk_manager import DiskManager
 from source.json_to_db_data_converter import convert_json_to_db
 from source.ocr_data_parser import OCRDataParser
 from source.ocr_image_processor import OCRImageProcessor
@@ -7,16 +9,23 @@ from source.screen_scanner import ScreenScanner
 
 
 def main():
-    suffix = "" # "_test"
+    suffix = "_zhuyuan"  # "_test"
 
-    images_path = "../images"
+    images_path = "../images" + suffix
     raw_json_path = "../output/raw_data" + suffix + ".json"
     disk_json_path = "../output/disk_data" + suffix + ".json"
     database_path = "../db/disk_database" + suffix + ".db"
 
     # Screenshot and save all the images on the game screen
-    # screen_scanner = ScreenScanner(images_path)
-    # screen_scanner.capture_and_save_disk_images()
+    while True:
+        screen_scanner = ScreenScanner(images_path)
+        screen_scanner.capture_and_save_disk_images()
+        # Ask the user if they want to continue
+        user_input = input("Do you want to scan again? (yes/no) (y/n): ").strip().lower()
+        if user_input != 'yes' or user_input != 'y' or user_input != 'n' or user_input != 'no':
+            print("Invalid input. Please enter 'yes' or 'no'.")
+        if user_input == 'no' or user_input == 'n':
+            break
 
     # Process all images with OCR and dump the raw data into a JSON file
     image_processor = OCRImageProcessor()
@@ -29,5 +38,16 @@ def main():
     convert_json_to_db(disk_json_path, database_path)
 
 
+def evaluate_disks():
+    # Initialize DiskDatabase (assumes implementation exists)
+    db = DiskDatabase("../db/disk_database_zhuyuan.db")
+    disk_manager = DiskManager(db)
+
+    # Example: Add, rank, and display disks
+    ranked_disks = disk_manager.rank_disks()
+    disk_manager.display_ranking(ranked_disks)
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    evaluate_disks()
